5c99d75a0a71f9f1f0744aabb5dbe1c5b9c51ad7
Egor <555666741@mail.ru>
2026-09-15 21:25:51 +0500

    solve interface/task1

--- interface/task1/main.go
+++ interface/task1/main.go
@@ -1,1 +1,55 @@
 package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
+
+type Circle struct {
+	radius float64
+}
+
+func NewCircle(radius float64) *Circle {
+	return &Circle{radius: radius}
+}
+
+func (c *Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c *Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
+func NewRectangle(width, height float64) *Rectangle {
+	return &Rectangle{width: width, height: height}
+}
+
+func (r *Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+func (r *Rectangle) Perimeter() float64 {
+	return 2 * (r.width + r.height)
+}
+
+func main() {
+	circle := NewCircle(5)
+	rectangle := NewRectangle(5, 10)
+
+	fmt.Println("Circle S: ", circle.Area())
+	fmt.Println("Circle P: ", circle.Perimeter())
+
+	fmt.Println("Rectangle S: ", rectangle.Area())
+	fmt.Println("Rectangle P: ", rectangle.Perimeter())
+}
