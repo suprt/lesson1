e574c0d4f047560d629d39a5fe794699466cf51f
Egor <555666741@mail.ru>
2026-09-16 02:01:27 +0500

    solve maps/task2

---
+++ maps/task2/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+func WordFrequency(text string) map[string]int {
+	result := make(map[string]int)
+	fields := strings.FieldsFunc(strings.ToLower(text), func(r rune) bool { return !unicode.IsLetter(r) && !unicode.IsNumber(r) })
+	for _, field := range fields {
+		result[field]++
+	}
+	return result
+}
+
+func PrintWordFrequency(freq map[string]int) {
+	for word, count := range freq {
+		fmt.Printf("%s\t%d\n", word, count)
+	}
+}
+
+func main() {
+	text := "golang, is great and Golang is fast"
+	freq := WordFrequency(text)
+	PrintWordFrequency(freq)
+}
